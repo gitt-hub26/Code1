05f8e9689589b0067fcfb06051aa8af1e833ca86
gitt-hub26 <omisakint3@gmail.com>
2026-03-17 23:13:42 +0000

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,3 +1,6 @@
 
 // For now, it's just a placeholder.
 console.log("Greenfield Local Hub loaded");
+function confirmDelete() {
+  return confirm("Are you sure you want to delete this product?");
+}
